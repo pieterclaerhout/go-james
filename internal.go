65f0a5ea4d95f7eee9deae71f98cbc7c48851bed
Pieter Claerhout <pieter@yellowduck.be>
2019-10-14 09:47:34 +0200

    Added commands to get the project revision and branch name

--- internal.go
+++ internal.go
@@ -3,6 +3,7 @@ package james
 import (
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/pkg/errors"
 
@@ -15,14 +16,14 @@ var (
 	ErrEmptyCommand = errors.New("No command specified")
 )
 
-func (project Project) runCommand(cmdLine []string) error {
+func (project Project) createCommand(cmdLine []string) (*exec.Cmd, error) {
 
 	var cmdPath string
 	var cmdArgs []string
 
 	switch len(cmdLine) {
 	case 0:
-		return ErrEmptyCommand
+		return nil, ErrEmptyCommand
 	case 1:
 		cmdPath = cmdLine[0]
 		cmdArgs = make([]string, 0)
@@ -31,18 +32,77 @@ func (project Project) runCommand(cmdLine []string) error {
 		cmdArgs = cmdLine[1:]
 	}
 
-	log.Debug("Executing:", shellquote.Join(cmdLine...))
-
 	command := exec.Command(cmdPath, cmdArgs...)
 	command.Env = os.Environ()
+	command.Dir = project.Path
+
+	return command, nil
+
+}
+
+func (project Project) runCommandToStdout(cmdLine []string) error {
+
+	command, err := project.createCommand(cmdLine)
+	if err != nil {
+		return err
+	}
 
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 
+	log.Debug("Executing:", shellquote.Join(cmdLine...))
 	if err := command.Start(); err != nil {
 		return err
 	}
 
 	return command.Wait()
+
+}
+
+func (project Project) determineRevision() string {
+
+	cmdLine := []string{"git", "rev-parse", "--short", "HEAD"}
+
+	command, err := project.createCommand(cmdLine)
+	if err != nil {
+		if log.DebugMode {
+			log.Error(err)
+		}
+		return ""
+	}
+
+	log.Debug("Executing:", shellquote.Join(cmdLine...))
+	output, err := command.CombinedOutput()
+	if err != nil {
+		if log.DebugMode {
+			log.Error(err)
+		}
+	}
+
+	return strings.TrimSpace(string(output))
+
+}
+
+func (project Project) determineBranch() string {
+
+	cmdLine := []string{"git", "rev-parse", "--abbrev-ref", "HEAD"}
+
+	command, err := project.createCommand(cmdLine)
+	if err != nil {
+		if log.DebugMode {
+			log.Error(err)
+		}
+		return ""
+	}
+
+	log.Debug("Executing:", shellquote.Join(cmdLine...))
+	output, err := command.CombinedOutput()
+	if err != nil {
+		if log.DebugMode {
+			log.Error(err)
+		}
+	}
+
+	return strings.TrimSpace(string(output))
 
 }
