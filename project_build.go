bab04974638e394cfda628daf4f6cfb7b4e417ba
Pieter Claerhout <pieter@yellowduck.be>
2019-10-14 09:48:20 +0200

    Building the project now injects the version info

--- project_build.go
+++ project_build.go
@@ -16,21 +16,29 @@ func (project Project) DoBuild() error {
 		return err
 	}
 
+	ldFlags := config.Build.LDFlags
+	if revision := project.determineRevision(); revision != "" {
+		ldFlags = append(ldFlags, "-X", config.Project.Package+".version.Revision="+revision)
+	}
+	if branch := project.determineBranch(); branch != "" {
+		ldFlags = append(ldFlags, "-X", config.Project.Package+".version.Branch="+branch)
+	}
+
 	buildCmd := []string{"go", "build"}
 
 	if config.Build.OuputName != "" {
 		buildCmd = append(buildCmd, "-o", config.Build.OuputName)
 	}
 
-	if len(config.Build.LDFlags) > 0 {
-		buildCmd = append(buildCmd, "-ldflags", strings.Join(config.Build.LDFlags, " "))
+	if len(ldFlags) > 0 {
+		buildCmd = append(buildCmd, "-ldflags", strings.Join(ldFlags, " "))
 	}
 
 	buildCmd = append(buildCmd, config.Project.Entrypoint)
 
 	log.Info("Building:", config.Build.OuputName)
 
-	if err := project.runCommand(buildCmd); err != nil {
+	if err := project.runCommandToStdout(buildCmd); err != nil {
 		return err
 	}
 
