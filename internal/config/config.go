035d5faf5dcb86fafbf04b23fc70344a302f06ef
Pieter Claerhout <pieter@yellowduck.be>
2019-10-14 17:54:33 +0200

    Added the list of badges to the config struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,8 +3,10 @@ package config
 import (
 	"encoding/json"
 	"io/ioutil"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/pieterclaerhout/go-log"
 )
@@ -64,4 +66,55 @@ func NewConfigFromPath(path string) (Config, error) {
 func NewConfigFromDir(path string) (Config, error) {
 	configPath := filepath.Join(path, configFileName)
 	return NewConfigFromPath(configPath)
+}
+
+// Badges returns the list of badges which should be shown for the project
+func (config Config) Badges() []Badge {
+
+	packageName := strings.TrimRight(config.Project.Package, "/")
+	relativePackageName := strings.TrimPrefix(packageName, "github.com/")
+
+	badges := []Badge{}
+
+	goReportBadge := Badge{
+		Title: "Go Report Card",
+		Link:  "https://goreportcard.com/report/" + packageName,
+		Image: "https://goreportcard.com/badge/" + packageName,
+	}
+	badges = append(badges, goReportBadge)
+
+	docsBadge := Badge{
+		Title: "Documentation",
+		Link:  "http://godoc.org/" + packageName,
+		Image: "https://godoc.org/" + packageName + "?status.svg",
+	}
+	badges = append(badges, docsBadge)
+
+	if strings.HasPrefix(packageName, "github.com/") {
+
+		licenseBadge := Badge{
+			Title: "License",
+			Link:  "https://" + packageName + "/raw/master/LICENSE",
+			Image: "https://img.shields.io/badge/license-Apache%20v2-orange.svg",
+		}
+		badges = append(badges, licenseBadge)
+
+		versionBadge := Badge{
+			Title: "GitHub Version",
+			Link:  "https://badge.fury.io/gh/" + url.PathEscape(relativePackageName),
+			Image: "https://badge.fury.io/gh/" + url.PathEscape(relativePackageName) + ".svg",
+		}
+		badges = append(badges, versionBadge)
+
+		issuesBadge := Badge{
+			Title: "GitHub issues",
+			Link:  "https://" + packageName + "/issues",
+			Image: "https://img.shields.io/github/issues/" + relativePackageName + ".svg",
+		}
+		badges = append(badges, issuesBadge)
+
+	}
+
+	return badges
+
 }
