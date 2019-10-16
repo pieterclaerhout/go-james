01d46ad07c6cbc530ca7de2077b0137c054d11e3
Pieter Claerhout <pieter@yellowduck.be>
2019-10-16 15:03:03 +0200

    Added package docs

--- library.go
+++ library.go
@@ -1,3 +1,4 @@
+// Package james is your butler and helps you to create, build, debug, test and run your Go projects.
+//
+// Read all about it on https://github.com/pieterclaerhout/go-james
 package james
-
-// Nothing here, move on
